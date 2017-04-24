52dc4469a10302801a7997035f01ecd149d6449b
jbvx8 <jbvx8@mail.umkc.edu>
2017-04-24 16:12:46 -0500

    connecting android to tensor flow server

--- Source/TensorFlow/Retrain Inception Model/label_image.py
+++ Source/TensorFlow/Retrain Inception Model/label_image.py
@@ -1,35 +1,55 @@
 import tensorflow as tf, sys
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS, cross_origin
+import base64
 
-#image_path = sys.argv[1]
-#image_path = 'data/flower_photos/roses/269037241_07fceff56a_m.jpg'
-image_path = 'datasets/Fountain_photos/J.C. Nichols Memorial Fountain/4789886.jpg'
-# image_path = '676728-bigthumbnail.jpg'
+app = Flask(__name__)
 
-# Read in the image_data
-image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+@app.route('/api/predict', methods=['POST'])
+@cross_origin()
+def predict():
+    data = request.values['imageBase64']
+    dataArray = bytearray()
+    dataArray.extend(data.encode())
+    with open("imageToPredict.jpeg", "wb") as fh:
+        fh.write(base64.decodebytes(dataArray))
 
-# Loads label file, strips off carriage return
-label_lines = [line.rstrip() for line
-               in tf.gfile.GFile("datasets/output_labels.txt")]
-                #in tf.gfile.GFile("data/output_labels.txt")]
+    #image_path = 'datasets/Fountain_photos/J.C. Nichols Memorial Fountain/4789886.jpg'
+    # image_path = '676728-bigthumbnail.jpg'
+    image_path = 'imageToPredict.jpeg'
 
-# Unpersists graph from file
-with tf.gfile.FastGFile("datasets/output_graph.pb", 'rb') as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
-    _ = tf.import_graph_def(graph_def, name='')
+    # Read in the image_data
+    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
-with tf.Session() as sess:
-    # Feed the image_data as input to the graph and get first prediction
-    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+    # Loads label file, strips off carriage return
+    label_lines = [line.rstrip() for line
+                   in tf.gfile.GFile("output_labels.txt")]
+                    #in tf.gfile.GFile("data/output_labels.txt")]
 
-    predictions = sess.run(softmax_tensor, \
-                           {'DecodeJpeg/contents:0': image_data})
+    # Unpersists graph from file
+    with tf.gfile.FastGFile("datasets/output_graph.pb", 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
 
-    # Sort to show labels of first prediction in order of confidence
-    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+    with tf.Session() as sess:
+        # Feed the image_data as input to the graph and get first prediction
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
-    for node_id in top_k:
-        human_string = label_lines[node_id]
-        score = predictions[0][node_id]
-        print('%s (score = %.5f)' % (human_string, score))
+        predictions = sess.run(softmax_tensor, \
+                               {'DecodeJpeg/contents:0': image_data})
+
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+        output_string = []
+        output_score = []
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+            output_string.append(human_string)
+            output_score.append(score)
+            print('%s (score = %.5f)' % (human_string, score))
+    return jsonify(results=[output_string])
+
+if (__name__) == "__main__":
+    app.run(host='0.0.0.0', threaded=True)
